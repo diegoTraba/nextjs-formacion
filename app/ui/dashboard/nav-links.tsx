a2c1fc53a80984fdffdf6ce0feff3f90d59b9f0e
diegoTraba <traba95diego@gmail.com>
2024-11-25 17:01:24 +0100

    ultimos cambios

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -8,6 +8,8 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import { invoices } from '@/app/lib/placeholder-data';
+import path from 'path';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -23,20 +25,34 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
+  // var RutaPrincipal=""
+  // if(pathname.includes("invoices")){
+  //   RutaPrincipal="invoices"
+  // }
+  // else if(pathname.includes("customers")){
+  //   RutaPrincipal="invoices"
+  // }
+  // else{
+  //   RutaPrincipal="dashboard"
+  // }
   return (
     <>   
       {links.map((link) => {
         const LinkIcon = link.icon;
         const routeParts= pathname.split('/');
         const lastPartRoute= routeParts[routeParts.length - 1];
+        // Verificar si es ruta exacta o hija
+        const isActive =  pathname === link.href || pathname.startsWith(link.href + "/");
         return (
+          //link que tiene unas clases y si la ruta actual (pathname) termina igual que el href del enlace, se le añaden unas clases extra para destacar que en enlace se corresponde
+          //con la ruta actual en la que nos encontramos
           <Link
             key={link.name}
             href={link.href}
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': (pathname===link.href? pathname===link.href : pathname.includes(link.href)),
+                'bg-sky-100 text-blue-600': (pathname.endsWith(link.href)),
               },
             )}
           >
