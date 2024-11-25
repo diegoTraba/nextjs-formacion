15c0842bcf1ba9d094bf8971b46b82fcb4b3fd98
diegoTraba <traba95diego@gmail.com>
2024-11-25 16:30:25 +0100

    ulitmos cambios

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -27,6 +27,8 @@ export default function NavLinks() {
     <>   
       {links.map((link) => {
         const LinkIcon = link.icon;
+        const routeParts= pathname.split('/');
+        const lastPartRoute= routeParts[routeParts.length - 1];
         return (
           <Link
             key={link.name}
@@ -34,7 +36,7 @@ export default function NavLinks() {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
+                'bg-sky-100 text-blue-600': (pathname===link.href? pathname===link.href : pathname.includes(link.href)),
               },
             )}
           >
